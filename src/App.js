4400e5478f101b1125ea76cf76bb3b2212eaf9ef
theryancook <ryancook12@gmail.com>
2020-06-29 14:48:11 +1000

    implementing exact=true

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 import HomeView from "./HomeView";
 import CategorySelectionView from "./CategorySelectionView";
 import NewEntryView from "./NewEntryView";
@@ -8,16 +8,16 @@ class App extends Component {
     render() {
         return (
             <div>
-<BrowserRouter>
-    <div>
-        <Route path="/" component={HomeView} />
-        <Route path="/test" component={HomeView} />
-        <Route path="/category/test" component={HomeView} />
-        <Route path="/category" component={HomeView} />
-        <Route exact path="/category" component={CategorySelectionView} />
-        <Route exact path="/entry" component={NewEntryView} />
-    </div>
-</BrowserRouter>
+                <BrowserRouter>
+                    <div>
+                        <Link to="/">Home</Link>
+                        <Link to="/category">Category</Link>
+                        <Link to="/entry">Entry</Link>
+                        <Route exact path="/" component={HomeView} />
+                        <Route exact path="/category" component={CategorySelectionView} />
+                        <Route exact path="/entry" component={NewEntryView} />
+                    </div>
+                </BrowserRouter>
             </div>
         );
     };
