a4aae3662fde3e96fb7be4836fa804cde008a9ec
theryancook <ryancook12@gmail.com>
2020-06-29 14:18:42 +1000

    state based routing

--- src/App.js
+++ src/App.js
@@ -4,12 +4,36 @@ import CategorySelectionView from "./CategorySelectionView";
 import NewEntryView from "./NewEntryView";
 
 class App extends Component {
+
+    state = {
+        location: "home"
+    }
+
+    getView() {
+        const { location } = this.state;
+        switch(location) {
+            case "home":
+                return < HomeView />
+            case "categorySelection":
+                return < CategorySelectionView />
+            case "newEntry":
+                return < NewEntryView />
+            default:
+                return null
+        }
+    }
+
+    changeLocation = (location) => {
+        this.setState({ location });
+    }
+
     render() {
         return (
             <div>
-                <HomeView />
-                <CategorySelectionView />
-                <NewEntryView />
+                <button onClick={() => this.changeLocation("home")}>Home</button>
+                <button onClick={() => this.changeLocation("categorySelection")}>Category Selection</button>
+                <button onClick={() => this.changeLocation("newEntry")}>New Entry</button>
+                {this.getView()}
             </div>
         );
     };
