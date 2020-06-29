7fde5f7b43abc51b0917a11097b249d2f35e1488
theryancook <ryancook12@gmail.com>
2020-06-29 14:19:20 +1000

    router based routing

--- src/App.js
+++ src/App.js
@@ -1,39 +1,20 @@
 import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 import HomeView from "./HomeView";
 import CategorySelectionView from "./CategorySelectionView";
 import NewEntryView from "./NewEntryView";
 
 class App extends Component {
-
-    state = {
-        location: "home"
-    }
-
-    getView() {
-        const { location } = this.state;
-        switch(location) {
-            case "home":
-                return < HomeView />
-            case "categorySelection":
-                return < CategorySelectionView />
-            case "newEntry":
-                return < NewEntryView />
-            default:
-                return null
-        }
-    }
-
-    changeLocation = (location) => {
-        this.setState({ location });
-    }
-
     render() {
         return (
             <div>
-                <button onClick={() => this.changeLocation("home")}>Home</button>
-                <button onClick={() => this.changeLocation("categorySelection")}>Category Selection</button>
-                <button onClick={() => this.changeLocation("newEntry")}>New Entry</button>
-                {this.getView()}
+                <BrowserRouter>
+                    <div>
+                        <Route exact path="/" component={HomeView} />
+                        <Route exact path="/category" component={CategorySelectionView} />
+                        <Route exact path="/entry" component={NewEntryView} />
+                    </div>
+                </BrowserRouter>
             </div>
         );
     };
